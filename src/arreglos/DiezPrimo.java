f161334dc1e8aedd8fc399f511b9fe26346f492d
jpulecio-diseño <jpulecio@unimayor.edu.co>
2026-03-11 23:28:52 -0500

    feat: Primer ejercicio hechoi pero me gaste una hora tratando de soluciuonar un bug de paquetes que aun no logro resolver

---
+++ src/arreglos/DiezPrimo.java
@@ -0,0 +1,11 @@
+package arreglos;
+
+public class DiezPrimo {
+    int[] numerosPrimo = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
+
+    public void mostrarNumerosPrimo() {
+        for (int numeroprimo : numerosPrimo) {
+            System.out.println(numeroprimo);
+        }
+    }
+}
