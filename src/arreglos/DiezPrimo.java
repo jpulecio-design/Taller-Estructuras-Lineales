8b37e4757b92026c5838f66efdda5296b4599eda
jpulecio-diseño <jpulecio@unimayor.edu.co>
2026-03-16 00:02:17 -0500

    Agrega enunciado a DiezPrimo

--- src/arreglos/DiezPrimo.java
+++ src/arreglos/DiezPrimo.java
@@ -1,5 +1,8 @@
 package arreglos;
 
+/*  1.   Inicializar directamente un arreglo con los primeros 10 
+*números primos e imprimir el arreglo. 
+*/
 public class DiezPrimo {
     int[] numerosPrimo = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
 
